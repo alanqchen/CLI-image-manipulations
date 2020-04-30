06d2acc4f3dfb81b16e6b35bc803bac57f52525d
Alan Chen <alanchen1908@gmail.com>
2020-04-29 20:26:32 -0400

    Delete blur.h

--- blur.h
+++
@@ -1,7 +0,0 @@
-#include "pixel.h"
-#include "imageutil.h"
-
-bool checkNonNeg(int* mask, int maskwidth);
-pixel_t checkValid(pixel_t* pixels, int index, int width, int height,
-int* mask, int maskWidth);
-pixel_t* blur(pixel_t* pixels,int width,int height,int* mask,int maskwidth);
